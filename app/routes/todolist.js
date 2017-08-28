265bb8dde7cee53d5503f8676d6d1cbba1365369
znergy <ryanjonesirl@gmail.com>
2017-08-27 19:40:35 -0700

    add a populateDB function which will fire three insert functions using async.parallel. The responses from each insert are then combined into a single object which is logged to the console.

--- app/routes/todolist.js
+++ app/routes/todolist.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
-const Todolist = require('../models/todolist');
+const mongoose = require('mongoose')
+const Todolist = require('../models/todolist')
+const List = require('../data/sample-data')
+const async = require('async')
 
 // get all todolists
 function getTodolists(req, res) {
@@ -35,6 +37,50 @@ function updateTodolist(req, res) {
     })
   })
 }
+// populate db
+function populateDB() {
+  Todolist.remove({}, () => {
+    // wipe collection
+  })
+
+  async.parallel([
+    (callback) => {
+      let newTodolist = new Todolist({
+        username: "ZippyTheZipster",
+        title: "Work",
+        category: "programming",
+        tasks: ["code", "eat", "sleep"]
+      })
+      newTodolist.save((err, todolist) => {
+        callback(null, todolist)
+      })
+    },
+    (callback) => {
+      let newTodolist = new Todolist({
+        username: "TreeTrunk12",
+        title: "Homework",
+        category: "school",
+        tasks: ["ECON project", "Spanish paper", "Math"]
+      })
+      newTodolist.save((err, todolist) => {
+        callback(null, todolist)
+      })
+    },
+    (callback) => {
+      let newTodolist = new Todolist({
+        username: "Wally",
+        title: "Chores",
+        category: "chores",
+        tasks: ["Take out trash", "Wash dishes", "Laundry"]
+      })
+      newTodolist.save((err, todolist) => {
+        callback(null, todolist)
+      })
+    }
+  ], function(err, results) {
+    console.log({message: 'Database has been populated!', todolists: results})
+  })
+}
 
 // exporting functions to access in app.js
-module.exports = { getTodolists, getTodolist, postTodolist, deleteTodolist, updateTodolist }
+module.exports = { getTodolists, getTodolist, postTodolist, deleteTodolist, updateTodolist, populateDB }
